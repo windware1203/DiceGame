e84903064fbd13545e9c661cf34b110d896160b9
Takeda Nana <40207168+windware1203@users.noreply.github.com>
2020-10-22 16:03:41 +0800

    Update DiceTest.java

--- DiceTest.java
+++ DiceTest.java
@@ -1,11 +1,37 @@
 
 class DiceGame
 {
-    boolean check = true;
-    while(check)
+    public static void main(String[] args)
     {
-        Dice dice1 = new Dice();
-        Dice dice2 = new Dice();
-        int sum = dice1.getFace() + dice2.getFace();
+        boolean check = true;
+        while(check)
+        {
+            Dice dice1 = new Dice();
+            Dice dice2 = new Dice();
+            int sum = dice1.getFace() + dice2.getFace();
+            boolean winCheck = false;
+            switch (sum)
+            {
+                case 7:
+                    check = false;
+                    winCheck = true;
+                    break;
+                case 11:
+                    check = false;
+                    winCheck = true;
+                    break;
+                case 2:
+                    check = false;
+                    break;
+                case 3:
+                    check = false;
+                    break;
+                case 12:
+                    check = false;
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
