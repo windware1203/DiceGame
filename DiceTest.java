f467e9cdd982219a42212fd6c078e28b9eb4c21b
Takeda Nana <40207168+windware1203@users.noreply.github.com>
2020-10-22 15:51:39 +0800

    Update DiceTest.java

--- DiceTest.java
+++ DiceTest.java
@@ -1,1 +1,11 @@
 
+class DiceGame
+{
+    boolean check = true;
+    while(check)
+    {
+        Dice dice1 = new Dice();
+        Dice dice2 = new Dice();
+        int sum = dice1.face() + dice2.face();
+    }
+}
