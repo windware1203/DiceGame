c3419e4fe6a2f95115fc78a070c222fe7c91e5e4
Takeda Nana <40207168+windware1203@users.noreply.github.com>
2020-10-22 15:46:50 +0800

    Update Dice.java

--- Dice.java
+++ Dice.java
@@ -1,1 +1,17 @@
-
+/**
+  *takeda
+  *v.1
+  */
+import java.security.*;
+public class Dice {
+	private final int face;
+	public Dice()
+	{
+		SecureRandom random = new SecureRandom();
+		face = random.nextInt(6)+1;
+	}
+	public String toString()
+	{
+		return String.valueOf(face);
+	}
+}
